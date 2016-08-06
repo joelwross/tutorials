5b295bca17a6ab2196a809091f5d29be8a1e02e6
Dave Stearns <dlsinfo@uw.edu>
2016-08-06 15:03:39 -0700

    added target param to deploy script; cleanup in gulpfile

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
-echo "building to build/..."
-gulp
-echo "copying files from build/ to faculty web server..."
-rsync -azP ./build/ dlsinfo@ovid.u.washington.edu:public_html/tutorials
+if [ -n "$1" ]; then
+    echo "building to build/..."
+    gulp
+    echo "copying files from build/ to $1..."
+    rsync -azP ./build/ $1
+else
+    echo "Usage:"
+    echo "  ./deply.sh remote-destination"
+    echo ""
+    echo "For example:"
+    echo "  ./deploy.sh your-netid@ovid.u.washington.edu:public_html/tutorials"
+    exit 1
+fi
--- gulpfile.js
+++ gulpfile.js
@@ -13,20 +13,24 @@ var template = handlebars.compile(fs.readFileSync('./src/template.html', {encodi
 function extractTitleFromHTML(file) {
     return new Promise(function(resolve, reject) {
         var inH1 = false;
-        var extracted = false;
+        var title = "";
         var parser = new htmlparser.Parser({            
             onopentag(name, attrs) {
                 inH1 = ('h1' == name.toLowerCase());
             },
             ontext(text) {
                 if (inH1) {
-                    extracted = true;
-                    resolve(text);
+                    title += text;
+                }
+            },
+            onclosetag(name) {
+                if ('h1' == name.toLowerCase()) {
+                    resolve(title);
                 }
             },
             onend() {
-                if (!extracted) {
-                    reject(new Error('Unable to find an <h1> element in the source file ' + file.path));
+                if (title.length == 0) {
+                    reject(new Error('Unable to find an <h1> with content in the source file ' + file.path));
                 }
             }
         }, {decodeEntities: true});
@@ -36,17 +40,13 @@ function extractTitleFromHTML(file) {
     });
 }
 
-function extractTitle(file) {
-    return extractTitleFromHTML(file);
-}
-
 function mergeHTML(file, enc, cb) {
-    extractTitle(file).then(title => {
+    extractTitleFromHTML(file).then(title => {
         var merged = template({
             title: title,
-            content: fs.readFileSync(file.path, {encoding: 'utf8'})
+            content: file.contents.toString('utf8')
         });
-        file.contents = new Buffer(merged);
+        file.contents = Buffer.from(merged, 'utf8');
         cb(null, file);
     })
     .catch(err => {
@@ -54,28 +54,24 @@ function mergeHTML(file, enc, cb) {
     });
 }
 
-function merge(file, enc, cb) {
-    mergeHTML(file, enc, cb);
-}
-
-gulp.task('merge', function() {
+gulp.task('merge-html', () => {
     return gulp.src('./src/*/*.html')
-        .pipe(through.obj(merge))
+        .pipe(through.obj(mergeHTML))
         .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('images', function() {
+gulp.task('images', () => {
     return gulp.src('./src/*/img/**')
         .pipe($.cache($.imagemin()))
         .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('global-css', function() {
+gulp.task('global-css', () => {
     return gulp.src('./src/css/**')
         .pipe(gulp.dest(BUILD_DIR + '/css'));
 });
 
-gulp.task('global-img', function() {
+gulp.task('global-img', () => {
     return gulp.src('./src/img/**')
         .pipe($.cache($.imagemin()))
         .pipe(gulp.dest(BUILD_DIR + '/img'));
@@ -90,4 +86,4 @@ gulp.task('clean', () => {
     return del('./build');
 });
 
-gulp.task('default', ['merge', 'images', 'global-css', 'global-img', 'global-lib']);
+gulp.task('default', ['merge-html', 'images', 'global-css', 'global-img', 'global-lib']);
